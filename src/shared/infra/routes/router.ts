ebfcdf48a6da157947e7272a632b432799c4d1d7
josuerx12 <josueaze12@gmail.com>
2025-04-26 16:43:21 -0300

    :hammer:refact: add routes of auth.

--- src/shared/infra/routes/router.ts
+++ src/shared/infra/routes/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import instanceRouter from "./instance/instance.route";
 import userRouter from "./user/user.route";
+import authRouter from "./auth/auth.route";
 
 const router = Router();
 
 router.use("/instance", instanceRouter);
 router.use("/users", userRouter);
+router.use("/auth", authRouter);
 
 export default router;
